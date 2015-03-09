33fe43dd604ffaced57f23a12771b62b70e94af4
Siewier <przemeksiewierski@gmail.com>
2015-03-09 18:40:49 +0100

    marzec 2015

---
+++ Wizytowka/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Wizytowka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrzemekSiewierski ps 
+                = new PrzemekSiewierski();
+            Console.WriteLine(ps.ToString() 
+                + "\n" + ps.telefon().ToString() 
+                + "\n" + ps.eMail() 
+                + "\n" + ps.webPage());
+            Console.ReadLine();
+        }
+    }
+    class PrzemekSiewierski
+    {
+        public int telefon()
+        {
+            return 510105908;
+        }
+        public string eMail()
+        {
+            return 
+                "przemeksiewierski@gmail.com";
+        }
+        public string webPage()
+        {
+            return 
+                "przemeksiewierski.eu";
+        }
+    }
+}
+
